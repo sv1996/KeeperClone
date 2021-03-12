db19684347b6e8403ad94503e8dae00b04e90aac
SHIVAM VISHWAKARMA <vishwakshivam@gmail.com>
2021-03-12 18:10:46 +0530

    L

---
+++ build/precache-manifest.7c823387a1c66267c937c43d21e4a26b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e604cfae5187a0332f20c39ba99388ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "716e347a9f896567d424",
+    "url": "/static/js/2.a112150c.chunk.js"
+  },
+  {
+    "revision": "aa65fda60079544c791e",
+    "url": "/static/js/main.c5dc6797.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
